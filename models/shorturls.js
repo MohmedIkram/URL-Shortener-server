f8b4015d9862e738c8fae607b1b9d7de570e89fc
MohmedIkram <53212527+MohmedIkram@users.noreply.github.com>
2021-08-02 12:05:54 +0530

    Add files via upload

--- models/shorturls.js
+++ models/shorturls.js
@@ -1,1 +1,21 @@
+import mongoose from "mongoose";
+import shortid from "shortid";
 
+const UrlSchema = new mongoose.Schema({
+  FullUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    default: shortid.generate,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+export const ShortUrl = mongoose.model("ShortUrl", UrlSchema);
