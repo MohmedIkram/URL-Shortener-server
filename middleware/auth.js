f4cdf72d64ba8d891c7f59743a0c3033ca03f28c
Mohmed Ikram <53212527+MohmedIkram@users.noreply.github.com>
2021-08-13 15:05:24 +0530

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,9 @@ import jwt from "jsonwebtoken";
 // custom middleware
 export const auth = (req, res, next) => {
   try {
-    // const autorization = req.header("Authorization");
-    // console.log(autorization, autorization.split(" ")[1]);
     const token = req.header("x-auth-token");
     console.log(token);
-    jwt.verify(token, process.env.SECERET_KEY);
+    jwt.verify(token, "secretkey");
     next(); // OK
   } catch (err) {
     res.status(500);
@@ -15,5 +13,3 @@ export const auth = (req, res, next) => {
     res.send({ err: err.message });
   }
 };
-
-// Login -> token -> store it in localStorage -> fetch call set header
